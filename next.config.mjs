3571bc2b856e565d804da8b0f45b1943a0a2c449
Chaitya <chaitanyaasole@gmail.com>
2024-03-18 22:35:31 +0530

    Add Pdf view and Edge Store

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+        // If the build target is server-side, configure node-loader for .node files
+        if (isServer) {
+            config.module.rules.push({
+                test: /\.node$/,
+                loader: 'node-loader',
+            });
+        }
 
+        return config;
+    },
+};
+
+// module.exports = nextConfig;
 export default nextConfig;
